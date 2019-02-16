52d09e0ad9475b01ef648a82a3f1261001d6b1a1
Colton Potter <cpotter2@oberlin.edu>
2019-02-16 15:54:48 -0500

    Add user creation function & dependencies

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const firebase = require('firebase');
 admin.initializeApp(functions.config().firebase);
 
-
 const addItem = item => {
-console.log(item);
+  console.log(item);
   return admin
     .firestore()
     .collection('items')
@@ -12,7 +12,23 @@ console.log(item);
     .then(doc => console.log('notification added', doc));
 };
 
+const testUser = email => {
+  console.log(email);
+  const password = 'password';
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      console.log(error.message);
+    });
+};
+
 exports.addItem = functions.https.onRequest((request, response) => {
-//   const itemName = request.query.itemName;
+  //   const itemName = request.query.itemName;
   return addItem(request.query).then(() => response.end('200'));
+});
+
+exports.testUser = functions.https.onRequest((req, res) => {
+  console.log('Request is ' + req);
+  return testUser(req.query).then(() => res.end('200'));
 });
