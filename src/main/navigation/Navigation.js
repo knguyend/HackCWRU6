bf06d5cdb20f7b9ec6c7761b81fe70fbe47d0c7f
Minh Lam <mlam@oberlin.edu>
2019-02-16 18:03:38 -0500

    feat: Navbar done

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
-const Navigation = () => {
-    return (
-        <div> 
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/">Profile</NavLink>
-            //Textfield
-            //SearchButton
-        </div>
-    )
-}
+const NavBar = props => {
+  // TODO(ML): Add back only show appropriate links when signed in
+  //   const sideLinks = props.loggedIn ? <SignedInLinks /> : <SignedOutLinks />;
+  const sideLinks = true ? <SignedInLinks /> : <SignedOutLinks />;
+  return (
+    <nav className="nav-wrapper" style={{ backgroundColor: '#507642' }}>
+      <div className="container">
+        <Link to="/" style={{fontSize: 38}}>
+            <em>Twine</em>
+        </Link>
+        {sideLinks}
+      </div>
+    </nav>
+  );
+};
 
-export default Navigation;
+export default NavBar;
---
+++ src/main/navigation/SignedInLinks.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInLinks = props => {
+  return (
+    <ul className="right">
+      <li>
+        <a onClick={alert('this has not been implemented yet')}>
+          Sign Out
+        </a>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating" style={{backgroundColor: '#A37B45'}}>
+          {'lamchiminhmark@gmail.com'.substring(0, 2).toUpperCase()}
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
