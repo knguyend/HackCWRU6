4e41f0d8f9dae20892f4e36f66e468137f13e905
Minh Lam <mlam@oberlin.edu>
2019-02-16 12:14:13 -0500

    feat: Added server side handling of GET

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const addItem = item => {
+console.log(item);
+  return admin
+    .firestore()
+    .collection('items')
+    .add(item)
+    .then(doc => console.log('notification added', doc));
+};
+
+exports.addItem = functions.https.onRequest((request, response) => {
+//   const itemName = request.query.itemName;
+  return addItem(request.query).then(() => response.end('200'));
+});
