c514173178eb29f910cbd5b8c1933fead456e3f5
Minh Lam <mlam@oberlin.edu>
2019-02-16 17:14:50 -0500

    feat: Added testUser method on the back end

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,14 @@ const admin = require('firebase-admin');
 const firebase = require('firebase');
 admin.initializeApp(functions.config().firebase);
 
+var config = {
+  apiKey: 'AIzaSyDbJE9Wer9vcQUOajs-AqIRQLCj9ZxnPdg',
+  authDomain: 'twine-788ae.firebaseapp.com',
+  databaseURL: 'https://twine-788ae.firebaseio.com',
+  storageBucket: 'twine-788ae.appspot.com'
+};
+firebase.initializeApp(config);
+
 const addItem = item => {
   console.log(item);
   return admin
@@ -12,9 +20,8 @@ const addItem = item => {
     .then(doc => console.log('notification added', doc));
 };
 
-const testUser = email => {
+const testUser = ({ email, password }) => {
   console.log(email);
-  const password = 'password';
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
