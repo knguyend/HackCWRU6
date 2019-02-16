e33be61aaef74fa103c046a0a8956fe02ec1b644
Colton Potter <cpotter2@oberlin.edu>
2019-02-16 18:41:21 -0500

    Incremental Style Changes

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import "./Main.css"
 import Items from './homepage/Items';
+import styled from 'styled-components';
 
 class Main extends React.Component {
   state = {
--- src/main/homepage/EachItem.js
+++ src/main/homepage/EachItem.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React from 'react';
 //import "./EachItem.css";
+import guitar from './guitar.png';
+import styled from 'styled-components';
+
+const Li = styled.li`
+  list-style-type: none;
+  height: 33%;
+  width: 33%;
+
+  .item-wrapper {
+    border: 3px solid green;
+    height: 100%;
+  }
+
+  .imgDiv {
+    height: 67%;
+    background-image: url(${guitar});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+
+  .item-info {
+    height: 33%;
+  }
+
+  h2 {
+    margin: 0px;
+    font-size: 2em;
+  }
+`;
 
 const EachItem = ({ item }) => {
   const { id, title, description, photoUrl, condition, ownerId } = item;
 
   return (
-    <li className="item">
-      <img src={photoUrl} alt={title} />
-      <div className="item-title">
-        <h2>{title}</h2>
-        <section className="item-details">
-          <div className="item-description">
-            <span className="description"></span>
-            <span>{description}</span>
-          </div>
-          <div className="item-condition">
-            <span className="condition">Condition: </span>
-            <span>{condition}</span>
-          </div>
-        </section>
+    <Li className="item">
+      <div className="item-wrapper">
+        <div className="imgDiv" />
+        <div className="item-info">
+          <h2>{title}</h2>
+          <span className="description">{description}</span>
+          <br />
+          <span className="condition">Condition: {condition}</span>
+        </div>
       </div>
-    </li>
+    </Li>
   );
 };
 
