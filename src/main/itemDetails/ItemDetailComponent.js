0d3522f48cfa56a2bd0325ed5972774d0569ae9c
Minh Lam <mlam@oberlin.edu>
2019-02-17 04:42:41 -0500

    feat: Item details contact info all ready

--- src/main/itemDetails/ItemDetailComponent.js
+++ src/main/itemDetails/ItemDetailComponent.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import Contacts from './Contacts';
+import {Link} from 'react-router-dom';
 import './ItemDetails.css';
 
-const ItemDetailComponent = ({ data }) => {
-    console.log(data.title);
-    return (
-        <div className="grid-container">
-            <div className="Photo"> <img src={data.photoURL} alt={data.title}/> </div>
-            <div className="Description">
-                <h2>{data.title}</h2>
-                <p>{data.description}</p>
-            </div>  
-            <div className="Contact"><h2>Contact</h2>
-                <p>{data.ownerId}</p>
-            </div>
+const firebase = window.firebase;
 
-        </div>
-    )
+const ItemDetailComponent = ({ data }) => {
+  return (
+    <div className="grid-container">
+      <div className="Photo">
+        {' '}
+        <img src={data.photoURL} alt={data.title} />{' '}
+      </div>
+      <div className="Description">
+        <h2>{data.title}</h2>
+        <p>{data.description}</p>
+      </div>
+      <div className="Contact">
+        <h2>Contact</h2>
+        {firebase.auth().getUid() ? (
+          <Contacts data={data} />
+        ) : (
+          <p>
+            <Link to="/sign-in">Sign In</Link> or{' '}
+            <Link to="/sign-up">Sign Up</Link> to see contact information
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ItemDetailComponent;
